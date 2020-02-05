527157e1f060e903bc043f2fa3fd8d4bceaa6586
Sabbir <SabbirMac@labs-mac.local>
2020-02-05 09:44:52 +0600

    Initial Commit

---
+++ Notification Observer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notification Observer
+//
+//  Created by Sabbir on 2/5/20.
+//  Copyright © 2020 Sabbir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
